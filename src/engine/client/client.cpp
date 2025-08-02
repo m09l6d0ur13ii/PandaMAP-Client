4de8e37f20f09f0f5daebe49ca1eb2f2c3645ee9
ChillerDragon <chillerdragon@gmail.com>
2025-08-02 13:31:32 +0200

    Oops fix build

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -4937,7 +4937,7 @@ int main(int argc, const char **argv)
 			char aError[2048];
 			snprintf(aError, sizeof(aError), "Failed to load config from '%s'.", s_aConfigDomains[ConfigDomain].m_aConfigPath);
 			log_error("client", "%s", aError);
-			pClient->ShowMessageBox({.m_pTitle = "Config File Error", .m_pMessage = pError});
+			pClient->ShowMessageBox({.m_pTitle = "Config File Error", .m_pMessage = aError});
 			PerformAllCleanup();
 			return -1;
 		}

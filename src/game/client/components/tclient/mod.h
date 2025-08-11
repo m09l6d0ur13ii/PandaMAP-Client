9b3bfd34da72367ad32982084f8820a4b0058376
SollyBunny <sollybunn@gmail.com>
2025-08-11 18:20:30 +0000

    Remove mod_rcon_*

--- src/game/client/components/tclient/mod.h
+++ src/game/client/components/tclient/mod.h
@@ -8,17 +8,7 @@
 
 class CMod : public CComponent
 {
-private:
-	class CIden;
-
 public:
-	CIden IdenFromStr(const char *pStr, bool IsName);
-	int TimeFromStr(const char *pStr, char Unit);
-
-	void Kill(const CIden &Iden, bool Silent);
-	void Kick(const CIden &Iden, const char *pReason);
-	void Ban(const CIden &Iden, const char *pTime, const char *pReason);
-	void Mute(const CIden &Iden, const char *pTime, const char *pReason);
 
 	int m_ModWeaponActiveId = -1;
 	float m_ModWeaponActiveTimeLeft;

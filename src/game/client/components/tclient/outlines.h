f80d35719ed4d634efef58d1c63b98ab8b1d29b6
SollyBunny <sollybunn@gmail.com>
2025-09-14 13:20:24 +0000

    Inline and simplify stuff in outlines (removes tele rendering for now)

--- src/game/client/components/tclient/outlines.h
+++ src/game/client/components/tclient/outlines.h
@@ -8,10 +8,6 @@ class CTeleTile;
 
 class COutlines : public CComponent
 {
-private:
-	void RenderGameTileOutlines(CTile *pTiles, int w, int h, float Scale, int TileType) const;
-	void RenderTeleOutlines(CTile *pTiles, CTeleTile *pTele, int w, int h, float Scale) const;
-
 public:
 	int Sizeof() const override { return sizeof(*this); }
 	void OnConsoleInit() override;

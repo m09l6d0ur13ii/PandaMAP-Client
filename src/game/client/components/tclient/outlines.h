a9e512ac5739f6b584a497d79d08dd7ca7ecf812
SollyBunny <sollybunn@gmail.com>
2025-09-14 13:05:59 +0000

    Move outline rendering to outlines.cpp

--- src/game/client/components/tclient/outlines.h
+++ src/game/client/components/tclient/outlines.h
@@ -3,8 +3,15 @@
 
 #include <game/client/component.h>
 
+class CTile;
+class CTeleTile;
+
 class COutlines : public CComponent
 {
+private:
+	void RenderGameTileOutlines(CTile *pTiles, int w, int h, float Scale, int TileType) const;
+	void RenderTeleOutlines(CTile *pTiles, CTeleTile *pTele, int w, int h, float Scale) const;
+
 public:
 	int Sizeof() const override { return sizeof(*this); }
 	void OnConsoleInit() override;
